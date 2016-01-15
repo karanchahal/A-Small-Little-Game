b5b13b6b2309e9bc77fc345c87fd295b87c60322
Karanbir Chahal <karanbleep@gmail.com>
2016-01-16 00:26:59 +0530

    Added better projectile collision.Eps 80 !

--- src/com/karan/rain/graphics/entity/mob/projectile/particle/Particle.java
+++ src/com/karan/rain/graphics/entity/mob/projectile/particle/Particle.java
@@ -17,7 +17,8 @@ public class Particle extends Entity{
     private int life;
     private int time= 0;
 
-    protected double xx,yy,xa,ya;
+    protected double xx,yy,zz;
+    protected double xa,ya,za;
 
     public Particle(int x,int y,int life) {
         sprite = Sprite.particle_normal;
@@ -29,8 +30,12 @@ public class Particle extends Entity{
 
         this.life = life + random.nextInt(50) - 20;
 
-        this.xa = random.nextGaussian(); // b/w -1 and 1 will give you a normal distribution more likely to be near 0 than 1 and -1
+        this.xa = random.nextGaussian() + 1.8; // b/w -1 and 1 will give you a normal distribution more likely to be near 0 than 1 and -1
+        if(this.xa < 0) {
+            xa = 0.1;
+        }
         this.ya = random.nextGaussian();
+        this.zz = random.nextFloat() + 2.0;
     }
 
     public Particle(int x,int y,int life,int amount) {
@@ -48,13 +53,22 @@ public class Particle extends Entity{
         if(time > life) {
             remove();
         }
+        za -= 0.1;
+
+        if(zz < 0) {
+            zz = 0;
+            za *= -0.5;
+            xa *= 0.4;
+            ya *= 0.4;
+        }
 
         this.xx += xa;
         this.yy += ya;
+        this.zz += za;
     }
 
     public void render(Screen screen) {
-        screen.renderSprite((int)xx,(int)yy,sprite,true);
+        screen.renderSprite((int)xx - 12,(int)yy - (int)zz,sprite,true);
     }
 
 
